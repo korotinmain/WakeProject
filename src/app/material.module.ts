16b7ec11863d1e150d5b156890237b83223972bb
Denys Korotin <korotinmain@gmail.com>
2018-10-04 23:22:51 +0300

    Added login page

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule, MatCardModule, MatIconModule } from '@angular/material';
-import { MatCheckboxModule } from '@angular/material/typings/esm5/checkbox';
+import { MatCheckboxModule } from '@angular/material';
 
 const MATERIAL_MODULES = [
     MatInputModule,
